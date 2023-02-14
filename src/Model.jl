9861d02bca8b1814221e2b213b430cbe044cda23
Abhi <8083613+AbhimanyuAryan@users.noreply.github.com>
2023-02-15 00:43:48 +0530

    👔  added App struct [model]

--- src/Model.jl
+++ src/Model.jl
@@ -1,5 +1,22 @@
 module Model
 
+import Base: ==
 
+using StructTypes
+using Dates
+
+export App
+
+mutable struct App
+    id::Int64
+    name::String
+    # files tree
+    created::DateTime
+end
+
+==(x::App, y::App) = x.id == y.id
+App() = Album(0, "", Dates.now())
+App(name, created) = App(0, name, Dates.now())
+StructTypes.StructTypes(::Type{App}) = StructTypes.Mutable()
 
 end # module
